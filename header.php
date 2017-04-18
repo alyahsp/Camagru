de780de19baa5b5cc475e51cd2ebcca013368666
Stanya PALMARO <spalmaro@e2r5p5.42.fr>
2017-04-18 18:56:43 +0200

    update

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<header class="header">
+		<div class="logo">
+			<a href="index.php">
+				<img alt="Camagru" src="./Camagru.png">
+			</a>
+		</div>
+	<header>
+</html>
