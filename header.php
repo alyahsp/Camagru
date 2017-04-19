f1921cd955f1da9db7159265e6237b77af63be95
Stanya Palmaro <spalmaro@41.student.fr>
2017-04-19 02:47:24 +0200

    Made most of the front

--- header.php
+++ header.php
@@ -1,13 +1,27 @@
-<?php ?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>Camagru</title>
+		<link rel="stylesheet" href="css/header.css">
 	</head>
-	<header class="header">
-		<div class="logo">
-			<a href="index.php">
-				<img alt="Camagru" src="./Camagru.png">
-			</a>
+	<body>
+		<div class="header">
+			<div class="logo">
+				<a href="gallery.php">
+					<img alt="Camagru" src="img/Camagru.png">
+				</a>
+			</div>
+			<div class="logout">
+				<a href="index.php">
+					<img class="lgt" alt="Logout" src="img/logout.svg">
+				</a>
+			</div>
+			<div class="montage">
+				<a href="montage.php">
+					<img class="mnt" alt="Montage" src="img/montage.svg">
+				</a>
+			</div>
 		</div>
-	<header>
+	</body>
 </html>
---
+++ montage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Camagru - Montage</title>
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/montage.css">
+	</head>
+	<body>
+	<?php
+		include "header.php";
+	?>
+	<div class="divider">
+		<div class="main">
+		</div>
+		<div class="side">
+		</div>
+	</div>
+	<div class="footer">
+		<p class="login">spalmaro 2017</p>
+	</div>
+	</body>
+</html>
